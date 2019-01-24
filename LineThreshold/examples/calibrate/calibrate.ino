4170c1e2d259a99e19bc0d661bb6f1c3068d27db
Jacob Smith <jsmith2021@brandeis.edu>
2019-01-23 23:13:41 -0500

    updated example file

--- LineThreshold/examples/calibrate/calibrate.ino
+++ LineThreshold/examples/calibrate/calibrate.ino
@@ -1,18 +1,17 @@
-/*Jacob Smith 1/20/2019 Brandeis University Robotics Club
+/*Jacob Smith 1/23/2019 Brandeis University Robotics Club
  uses the LineThreeshold class to find the region the robot is over*/
 #include<LineCalibrator.h>
 #include<LineShield.h>
 #include<LineThreshold.h>
 
-//LineShield lineReader;
-//using parenthesis here doesn't work for some reason
+//everything the lineThreshold class needs to convert brightness readings to the names of regions
 int numRegions = 3;
 String regions[] = { "WHITE", "GRAY", "BLACK" };
 int thresholds[] = { 442, 1648 };
 String regionsSeen[NUM_SENSORS];
-//LineShield lineReader;
-LineThreshold thresh();
-
+//using parenthesis here doesn't work for some reason
+LineThreshold thresh;
+//runs once
 void setup() {
 	//starts serial connection at 9600 bits per second
 	Serial.begin(9600);
@@ -23,10 +22,14 @@ void setup() {
 	thresh.setElements(thresholds, regions, numRegions,
 			regionsSeen);
 }
-void loop() {
 
-	String region = thresh.getRegion();
-	displayLineReadings(region);
+//runs continously
+void loop() {
+	//convert the brightness readings to the names of regions
+	thresh.convertToRegion();
+	//display the names
+	displayLineReadings();
+	//wait 2 seconds
 	delay(2000);
 }
 
@@ -34,14 +37,11 @@ void loop() {
  * Displays what each individual sensor sees and the overall region
  * @param region the region the robot is on
  */
-void displayLineReadings(String region) {
+void displayLineReadings() {
 	Serial.print("Individual Sensors:\t");
-	//print what region each indiivual sensor sees
+	//print what region each individual sensor sees
 	for (int i = 0; i < NUM_SENSORS; i++) {
 		Serial.print(regionsSeen[i]);
 		Serial.print("\t");
 	}
-	//print the overall region
-	Serial.print("Final Region:\t");
-	Serial.println(region);
 }
