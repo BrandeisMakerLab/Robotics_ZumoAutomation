5dc82ef7d078f33c1a3df0fbf99748f32f0c833e
Ian Huang <ian880903@gmail.com>
2019-03-07 18:48:12 -0500

    CalibrateTimingFunction add testDistance and temp engineering notebook

--- Robotics Club Sketches/CalibrateTimingFunction/CalibrateTimingFunction.ino
+++ Robotics Club Sketches/CalibrateTimingFunction/CalibrateTimingFunction.ino
@@ -11,7 +11,8 @@ void setup() {
   Serial.begin(9600);
   Serial.println("ready");
   double robotSpeed = callibrate(71.65);
-  Serial.println(robotSpeed);
+  Serial.println(robotSpeed); //units: cm/s
+  testDistance(robotSpeed, 30.0);
 }
 
 void loop() {
@@ -19,6 +20,9 @@ void loop() {
   //robot name: WALL-E JR.
 } 
 
+/**
+ * Distance unit: centimeters
+ */
 double callibrate(double distance){
   timer.resetTime();
   while(!lineReader.isOnWhite()){
@@ -29,4 +33,12 @@ double callibrate(double distance){
   // put your setup code here, to run once:
   //While loop runs until sensor hits white
   //driveFoward should not have any arguments because there will be a lag.
+}
+
+/**
+ * This method test if the speed of the robot is correct by running the robot a certain distance.
+ * Parameters: distance unit: centimeters
+ */
+void testDistance(double robotSpeed, double distance) {
+  robot.driveForward(distance/robotSpeed);  //Drive the robot a certain time, which is (distance/robotSpeed)
 }
