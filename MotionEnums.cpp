9f92f22e752c6235907fa6b94a43a8fc16b59bdf
Jacob Smith <jsmith2021@brandeis.edu>
2019-04-09 23:08:15 -0400

    Worked on Wireless more

--- MotionEnums.cpp
+++ MotionEnums.cpp
@@ -1,12 +1,11 @@
 /*
- * MotionEnums.cpp
- *
- *  Created on: Apr 5, 2019
- *      Author: jsmit
+ * Enum Implementation of Drive Functions so many classes can use it
+ * Jacob Smith Brandeis Robotics Club
  */
 
 #include "MotionEnums.h"
 
+//creates a new Motion Enum Object
 MotionEnums::MotionEnums(int leftPowerMult, int rightPowerMult) {
         this->leftPowerMult = leftPowerMult;
         this->rightPowerMult=rightPowerMult;
--- MotionEnums.h
+++ MotionEnums.h
@@ -1,10 +1,10 @@
 /*
- * MotionEnums.h
- *
- *  Created on: Apr 5, 2019
- *      Author: jsmit
+ * Enum Implementation of Drive Functions so many classes can use it
+ * Jacob Smith Brandeis Robotics Club
+ * Modoifed from:https://stackoverflow.com/questions/1965249/how-to-write-a-java-enum-like-class-with-multiple-data-fields-in-c
  */
-s
+
+//set up the header file
 #ifndef MotionEnums_h
 #define MotionEnums_h
 #include<MotionEnums.h>
@@ -12,19 +12,24 @@ s
 class MotionEnums{
 
 	private:
+		//creates a new Motion Enum
 		MotionEnums(int leftPowerMult, int rightPowerMult);
+		//the left power multiplier
 		int leftPowerMult;
+		//the right power multiplier
 	    int rightPowerMult;
 	public:
 
-	    // Enum value DECLARATIONS - they are defined later
-	    static const MotionEnums driveForward	=MotionEnums(1,1);
-	    static const MotionEnums driveBackward	=MotionEnums(1,1);
-	    static const MotionEnums pivotRight		=MotionEnums(1,1);
-	    static const MotionEnums pivotLeft		=MotionEnums(1,1);
-	    static const MotionEnums turnRight		=MotionEnums(1,1);
-	    static const MotionEnums turnLeft		=MotionEnums(1,1);
-	    static const MotionEnums stopDrive		=MotionEnums(1,1);
+	    //static instances of all the possible tank Driving combinations
+	    static const MotionEnums driveBackward		=MotionEnums(-1,-1);
+	    static const MotionEnums pivotLeftBackward	=MotionEnums(-1,0);
+	    static const MotionEnums turnLeft			=MotionEnums(-1,1);
+	    static const MotionEnums pivotRightBackward	=MotionEnums(0,-1);
+	    static const MotionEnums driveStop			=MotionEnums(0,0);
+	    static const MotionEnums pivotLeftForward	=MotionEnums(-1,-1);
+	    static const MotionEnums turnRight			=MotionEnums(1,-1);
+	    static const MotionEnums pivotRightForward	=MotionEnums(1,0);
+	    static const MotionEnums driveForward		=MotionEnums(1,1);
 };
 
 #endif
